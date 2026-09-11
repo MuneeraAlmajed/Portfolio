d811b681f69fedea048c316f9bd4cc0e3651d0e2
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-09-11 16:02:28 +0300

    Update Portfolio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const heroContent = document.querySelector('.hero-content');
+const heroVisual = document.querySelector('.hero-visual');
+
+heroContent.classList.add('hero-animate');
+heroVisual.classList.add('hero-animate');
